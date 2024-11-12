343d68c34212958ff4a034cf8a163226655ed651
ovasam <samuovalle2020@gmail.com>
2024-11-11 19:04:34 -0500

    Finishing front-face, back-face, buttons and some styles..

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const $ = (t) => document.querySelector(t)
 
-const face = $('#facebook')
+const insta = $('#instagram')
+const whatsapp = $('#whatsapp')
+const inst = $('.insta')
+const whats = $('.whats')
 
-face.addEventListener('click', () => {
-    window.location.assign('https://www.youtube.com/') // <-- facebook de kevin
+insta.addEventListener('click', () => {
+    window.open('https://www.instagram.com/kevinmoreno_888/?igsh=MWlyc3hpbWlmc2kwdw%3D%3D', '_blank')
 })
+whatsapp.addEventListener('click', () => {
+    window.open('https://wa.me/qr/XEBYZ3TM6W2SM1', '_blank')
+})
+inst.addEventListener('click', () => {
+    window.open('https://www.instagram.com/kevinmoreno_888/?igsh=MWlyc3hpbWlmc2kwdw%3D%3D', '_blank')
+})
+whats.addEventListener('click', () => {
+    window.open('https://wa.me/qr/XEBYZ3TM6W2SM1', '_blank')
+})
+
 function voltearTarjeta() {
     const tarjeta = document.querySelector('.container');
     tarjeta.classList.toggle("volteada"); // Añade o quita la clase para girar
